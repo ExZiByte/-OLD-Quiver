599b1a62c2d0b2755134bdf86f1ebda1d227aa2b
ExZiByte <exzibyte@gmail.com>
2019-08-18 13:47:58 -0500

    testing the member information saver

--- src/main/java/nestedvar/Quiver/Listeners/Misc/QuiverJoin.java
+++ src/main/java/nestedvar/Quiver/Listeners/Misc/QuiverJoin.java
@@ -28,6 +28,7 @@ public class QuiverJoin extends ListenerAdapter {
 
         db.connect();
         MongoCollection guilds = db.getCollection("guilds");
+        MongoCollection members = db.getCollection("members");
 
         if (guilds.find(eq("guildID", event.getGuild().getId())).first() != null) {
             try {
@@ -55,17 +56,23 @@ public class QuiverJoin extends ListenerAdapter {
                 for (Member member : event.getGuild().getMembers()) {
                     if (!member.getUser().isBot()) {
                         BasicDBObject thing = new BasicDBObject(member.getUser().getId(), new BasicDBObject("name", member.getUser().getName() + "#" + member.getUser().getDiscriminator()).append("level", 0).append("xp", 0));
+                        if(members.find(eq("id", member.getUser().getId())).first() != null){
+
+                        } else {
+                            Document memberDoc = new Document("id", member.getUser().getId()).append("name", member.getUser().getName() + "#" + member.getUser().getDiscriminator()).append("facebook", "Not Set").append("instagram", "Not Set").append("youtube", "Not Set").append("twitch", "Not Set").append("mixer", "Not Set").append("imgur", "Not Set").append("tiktok", "Not Set").append("steam", "Not Set").append("blizzard", "Not Set").append("epic", "Not Set").append("twitter", "Not Set").append("origin", "Not Set").append("reddit", "Not Set").append("spotify", "Not Set").append("skype", "Not Set").append("xboxlive", "Not Set").append("psn", "Not Set").append("slack", "Not Set").append("snapchat", "Not Set").append("teamspeak", "Not Set").append("mumble", "Not Set").append("stackoverflow", "Not Set").append("tumblr", "Not Set").append("giphy", "Not Set");
+                            members.insertOne(memberDoc);
+                        }
                         memberInformation.add(thing);
                     }
                 }
-                Document doc = new Document("guildID", event.getGuild().getId())
+                Document guild = new Document("guildID", event.getGuild().getId())
                         .append("guildName", event.getGuild().getName())
                         .append("prefix", "Q!")
                         .append("members", memberInformation)
                         .append("isBlacklisted", false)
                         .append("isChannelSystemEnabled", true);
 
-                guilds.insertOne(doc);
+                guilds.insertOne(guild);
                 db.close();
             } catch (IOException e) {
                 e.printStackTrace();
