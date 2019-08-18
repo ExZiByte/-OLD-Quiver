5e3acb4cfb540ff838097994b05cc862eb7b3dca
ExZiByte <exzibyte@gmail.com>
2019-08-18 18:09:38 -0500

    Testing member join and being added to guild member list

--- src/main/java/nestedvar/Quiver/Listeners/Misc/MemberJoin.java
+++ src/main/java/nestedvar/Quiver/Listeners/Misc/MemberJoin.java
@@ -21,14 +21,20 @@ public class MemberJoin extends ListenerAdapter {
         db.connect();
         MongoCollection guilds = db.getCollection("guilds");
         MongoCollection members = db.getCollection("members");
-        if(members.find(eq("id", event.getMember().getUser().getId())).first() != null) {
+        if(guilds.find(eq("members." + event.getMember().getUser().getId())).first() != null){
 
-        } else {
+        } else{
             List<BasicDBObject> memberInformation = new ArrayList<>();
             if (!event.getMember().getUser().isBot()) {
                 BasicDBObject thing = new BasicDBObject(event.getMember().getUser().getId(), new BasicDBObject("name", event.getMember().getUser().getName() + "#" + event.getMember().getUser().getDiscriminator()).append("level", 0).append("xp", 0));
                 memberInformation.add(thing);
                 guilds.updateOne(eq("guildID", event.getGuild().getId()), new Document("$set", memberInformation));
+            }
+        }
+        if(members.find(eq("id", event.getMember().getUser().getId())).first() != null) {
+
+        } else {
+            if (!event.getMember().getUser().isBot()) {
                 Document memberDoc = new Document("id", event.getMember().getUser().getId()).append("name", event.getMember().getUser().getName() + "#" + event.getMember().getUser().getDiscriminator()).append("facebook", "Not Set").append("instagram", "Not Set").append("youtube", "Not Set").append("twitch", "Not Set").append("mixer", "Not Set").append("imgur", "Not Set").append("tiktok", "Not Set").append("steam", "Not Set").append("blizzard", "Not Set").append("epic", "Not Set").append("twitter", "Not Set").append("origin", "Not Set").append("reddit", "Not Set").append("spotify", "Not Set").append("skype", "Not Set").append("xboxlive", "Not Set").append("psn", "Not Set").append("slack", "Not Set").append("snapchat", "Not Set").append("teamspeak", "Not Set").append("mumble", "Not Set").append("stackoverflow", "Not Set").append("tumblr", "Not Set").append("giphy", "Not Set");
                 members.insertOne(memberDoc);
             }
